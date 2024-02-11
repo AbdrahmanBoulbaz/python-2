ae870da03ad1d50e8bb76435f134dce345c41a7c
AbderrahmanBoulbaz <elbouatmani3@gmail.com>
2024-02-11 23:38:30 +0100

    second commits

--- teste 1/exercice 1.py
+++
@@ -1,8 +0,0 @@
-op_list = ["Hello", "Word"]
-op1_tuple =("Hello", "Casablanca")
-op2_set = {"Hello", "tutu!"}
-op3_dict = {"Hello" : "titi!"}
-print(op_list )
-print(op1_tuple )
-print(op2_set)
-print(op3_dict)
--- teste 1/exercice 4.py
+++
@@ -1,7 +0,0 @@
-num = float(input("enter un nombre"))
-op = num % 2
-print(op)
-if num == 0.0 :
-    print("le nombre est double ")
-else: 
-    print("le nombre est Simple")
