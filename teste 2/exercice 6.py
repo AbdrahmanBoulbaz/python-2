cf12881175773c32cb6d0241bc199be393b5fad3
AbdrahmanBoulbaz <157428990+AbdrahmanBoulbaz@users.noreply.github.com>
2024-02-17 22:49:17 +0100

    Update exercice 6.py

--- teste 2/exercice 6.py
+++ teste 2/exercice 6.py
@@ -1,19 +1,61 @@
-import string
+cookbook = {
+  "Sandwich": {
+      "ingredients": ["ham", "bread", "cheese", "tomatoes"],
+      "meal": "lunch",
+      "prep_time": 10
+  },
+  "Cake": {
+      "ingredients": ["flour", "sugar", "eggs"],
+      "meal": "dessert",
+      "prep_time": 60
+  },
+  "Salad": {
+      "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
+      "meal": "lunch",
+      "prep_time": 15
+  }
+}
 
-def print_words_longer_than_n(S, N):
-    if not isinstance(S, str) or not isinstance(N, int):
-        print("Error: Both arguments must be a string and an integer.")
-        return
+def print_recipe_names():
+  print("Recipe Names:")
+  for recipe_name in cookbook:
+      print("- " + recipe_name)
 
-    S_cleaned = ''.join(char for char in S if char not in string.punctuation)
+def print_recipe_details(recipe_name):
+  if recipe_name in cookbook:
+      recipe = cookbook[recipe_name]
+      print(f"Details of {recipe_name}:")
+      print("Ingredients:", ", ".join(recipe["ingredients"]))
+      print("Meal:", recipe["meal"])
+      print("Preparation Time:", recipe["prep_time"], "minutes")
+  else:
+      print("Recipe not found in the cookbook.")
 
-    
-    words = S_cleaned.split()
-    filtered_words = [word for word in words if len(word) > N]
-    print(filtered_words)
+def delete_recipe(recipe_name):
+  if recipe_name in cookbook:
+      del cookbook[recipe_name]
+      print(f"{recipe_name} recipe deleted successfully.")
+  else:
+      print("Recipe not found in the cookbook.")
 
+def add_recipe():
+  name = input("Enter recipe name: ")
+  ingredients = input("Enter ingredients (separated by comma): ").split(", ")
+  meal = input("Enter meal type: ")
+  prep_time = int(input("Enter preparation time (in minutes): "))
+  cookbook[name] = {
+      "ingredients": ingredients,
+      "meal": meal,
+      "prep_time": prep_time
+  }
+  print(f"{name} recipe added successfully.")
 
-if __name__ == "__main__":
-    S = "Hello, world! This is a test string. It has some words longer than 3 characters."
-    N = 3
-    print_words_longer_than_n(S, N)
+print_recipe_names()
+print()
+print_recipe_details("Cake")
+print()
+delete_recipe("Sandwich")
+print()
+add_recipe()
+print()
+print_recipe_names()
