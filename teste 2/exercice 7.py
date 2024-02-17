d972435fca0ec6ac99a270f77114d01889f76175
AbdrahmanBoulbaz <157428990+AbdrahmanBoulbaz@users.noreply.github.com>
2024-02-17 22:51:47 +0100

    Update exercice 7.py

--- teste 2/exercice 7.py
+++ teste 2/exercice 7.py
@@ -1,27 +1,19 @@
-import sys 
-morse_code_dict = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
-    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
-    '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-    ' ': '/'
-}
-def encode_to_morse(text):
-    morse_code = 1
-    for char in text.upper():
-        if char in morse_code_dict:
-            morse_code.append(morse_code_dict[char])
-        else:
-            morse_code.append('')  
-    return ' '.join(morse_code)
+import string
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: python morse_code_encoder.py <text>")
+def print_words_longer_than_n(S, N):
+    if not isinstance(S, str) or not isinstance(N, int):
+        print("Error: Both arguments must be a string and an integer.")
         return
 
-    text = ' '.join(sys.argv[1:])
-    morse_code = encode_to_morse(text)
-    print(morse_code_dict['0'])
-print(morse_code_dict)
+    S_cleaned = ''.join(char for char in S if char not in string.punctuation)
+
+    
+    words = S_cleaned.split()
+    filtered_words = [word for word in words if len(word) > N]
+    print(filtered_words)
+
+
+if __name__ == "__main__":
+    S = "Hello, world! This is a test string. It has some words longer than 3 characters."
+    N = 3
+    print_words_longer_than_n(S, N)
