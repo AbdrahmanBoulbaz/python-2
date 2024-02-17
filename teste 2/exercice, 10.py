e313eee01a726d350731bec54f4a8194baf60ddb
AbdrahmanBoulbaz <157428990+AbdrahmanBoulbaz@users.noreply.github.com>
2024-02-17 22:55:20 +0100

    Update exercice, 10.py

--- teste 2/exercice, 10.py
+++ teste 2/exercice, 10.py
@@ -1,19 +1,7 @@
-cookbook = {
-    "Sandwich": {
-        "ingredients": ["ham", "bread", "cheese", "tomatoes"],
-        "meal": "lunch",
-        "prep_time": 10
-    },
-    "Cake": {
-        "ingredients": ["flour", "sugar", "eggs"],
-        "meal": "dessert",
-        "prep_time": 60
-    },
-    "Salad": {
-        "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
-        "meal": "lunch",
-        "prep_time": 15
-    }
-}
-
-print(cookbook["Sandwich"])
+def ft_progress(lst):
+    total_items = len(lst)
+    for index, item in enumerate(lst):
+        yield (index + 1) / total_items * 100, item
+my_list = range(100)
+for progress, item in ft_progress(my_list):
+    print(f"Progress: {progress:.2f}%, Current Item: {item}")
